6e4be60e30525e91878632412615b293dcff6486
VGorash <gorash590@gmail.com>
2021-04-08 13:56:50 +0300

    Wildfly deploy

--- src/main/java/com/blps/app/AppApplication.java
+++ src/main/java/com/blps/app/AppApplication.java
@@ -2,12 +2,20 @@ package com.blps.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(applicationClass);
+    }
+
+    private static final Class<AppApplication> applicationClass = AppApplication.class;
 }
