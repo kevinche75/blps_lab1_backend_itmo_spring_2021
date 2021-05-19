736328a593e89282c27f0a78c72cdc41a99ebc98
VGorash <gorash590@gmail.com>
2021-05-19 21:59:40 +0300

    Simple kafka

--- AirlineService/src/main/java/com/blps/airlineservice/AirlineServiceApplication.java
+++ AirlineService/src/main/java/com/blps/airlineservice/AirlineServiceApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@EnableKafka
 @SpringBootApplication
 public class AirlineServiceApplication extends SpringBootServletInitializer{
+
+    @KafkaListener(topics="flights")
+    public void msgListener(String msg){
+        System.out.println(msg);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AirlineServiceApplication.class, args);
--- MainApplication/src/main/java/com/blps/app/service/AirlineApiService.java
+++ MainApplication/src/main/java/com/blps/app/service/AirlineApiService.java
@@ -2,7 +2,9 @@ package com.blps.app.service;
 
 import com.blps.app.model.Flight;
 import com.blps.app.model.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +16,9 @@ public class AirlineApiService {
 
     @Value("${app.airline.url}")
     private String airlineUrl;
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
 
     public Long bookTicket(Flight flight){
         return new RestTemplate().postForObject(airlineUrl+"flight/{id}", "", Long.class, flight.getId());
@@ -32,6 +37,8 @@ public class AirlineApiService {
     }
 
     public List<Flight> findTickets(String from, String to, Date date){
+        template.send("flights", Long.toString(System.currentTimeMillis()), "FLIGHT SEARCH").addCallback(System.out::println, System.err::println);
+        template.flush();
         Flight[] flights = new RestTemplate()
                 .getForEntity(airlineUrl+"flights?from={from}&to={to}&date={date}", Flight[].class, from, to, new SimpleDateFormat("yyyy-MM-dd").format(date)).getBody();
         if(flights==null){
